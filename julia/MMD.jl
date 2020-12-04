212fad7df2ccc2ed15a9c5a91c0c984aabb2d202
Jon Cockayne <jcockayne@turing.ac.uk>
2020-12-04 10:16:23 +0000

    Fixed bad commit on PushforwardPlots. Adjusted the MMD experiments for the paper.

--- julia/MMD.jl
+++ julia/MMD.jl
@@ -7,46 +7,33 @@ using Distributed
 
 export mmd_kernel_factory, mmd, bootstrap_mmd
 
-function mmd_kernel_factory(input_samples::Matrix{Float64},
-                            output_samples::Matrix{Float64})
+function mmd_kernel_factory(input_samples::AbstractMatrix{Float64},
+                            output_samples::AbstractMatrix{Float64})
 
-    N = size(input_samples)[2]
-
-    len_dist = convert(Int64,(2*N-1)*(2*N)/2)
-
-    dist = Array{Float64}(undef,len_dist)
-    d = 1
+    combined_samples = vcat(input_samples, output_samples)
+    N = size(combined_samples, 1)
+    distances = Vector{Float64}()
     for i = 1:N
         for j = (i+1):N
-            dist[d] = norm(input_samples[:,i] - input_samples[:,j])
-            d = d + 1
-        end
-        for j = 1:N
-            dist[d] = norm(input_samples[:,i] - output_samples[:,j])
-            d = d + 1
+            distance = norm(combined_samples[i,:] - combined_samples[j,:])
+            append!(distances, [distance])
         end
     end
-    for i = 1:(N-1)
-        for j = (i+1):N
-            dist[d] = norm(output_samples[:,i] - output_samples[:,j])
-            d = d + 1
-        end
-    end
-    dist_mean = mean(dist)
+    length_scale = median(distances)
 
     function gauss_ker(x::Array{Float64,1},y::Array{Float64,1})
-        return exp.(-0.5.*(norm(x-y).^2)./dist_mean^2)
+        return exp.(-0.5.*(norm(x-y).^2)./length_scale^2)
     end
 
     return gauss_ker
 end
 
-function mmd(input_samples::Matrix{Float64}, output_samples::Matrix{Float64},
+function mmd(input_samples::AbstractMatrix{Float64}, output_samples::AbstractMatrix{Float64},
              kernel::Function)
     N = size(input_samples)[2]
 
     mmd_est = 0
-    
+
     for i = 1:(N-1)
         for j = (i+1):N
             mmd_est += (2*kernel(input_samples[:,i],input_samples[:,j])
@@ -60,12 +47,12 @@ function mmd(input_samples::Matrix{Float64}, output_samples::Matrix{Float64},
     return mmd_est
 end
 
-function mmd_p(input_samples::Matrix{Float64}, output_samples::Matrix{Float64},
+function mmd_p(input_samples::AbstractMatrix{Float64}, output_samples::AbstractMatrix{Float64},
                kernel::Function)
     N = size(input_samples)[2]
 
     mmd_est = 0
-    
+
     mmd_est = @distributed (+) for i = 1:(N-1)
         mmd_inner = 0
         for j = (i+1):N
@@ -81,24 +68,26 @@ function mmd_p(input_samples::Matrix{Float64}, output_samples::Matrix{Float64},
     return mmd_est
 end
 
-function bootstrap_mmd(input_samples::Matrix{Float64},
-                       output_samples::Matrix{Float64}, kernel::Function,
+function bootstrap_mmd(input_samples::AbstractMatrix{Float64},
+                       output_samples::AbstractMatrix{Float64}, kernel::Function,
                        N_mmd::Int64)
 
-    M = size(input_samples)[2]
-    combined_sample = [input_samples output_samples]
+    combined_sample = vcat(input_samples, output_samples)
+    N = size(input_samples, 1)
+    M = size(combined_sample, 1)
 
     mmd_list = Array{Tuple{Array{Float64,2},Array{Float64,2}},1}(undef,N_mmd)
 
     mmd_map = function (sample_tuple::Tuple{Array{Float64,2},Array{Float64,2}})
-        mmd(sample_tuple[1], sample_tuple[2],kernel)
+        mmd(sample_tuple[1], sample_tuple[2], kernel)
     end
 
     for i = 1:N_mmd
-        s1_index = sample(1:2*M,M,replace=false)
+        s1_index = sample(1:M, N, replace=true)
+        s2_index = sample(1:M, N, replace=true)
         #s2_index = sample(1:2*M,M)
-        not_in_s1(y) = !(y in s1_index)
-        s2_index = filter(not_in_s1,1:2*M)
+        #not_in_s1(y) = !(y in s1_index)
+        #s2_index = filter(not_in_s1,1:2*M)
 
         mmd_list[i] = (combined_sample[:,s1_index],
                        combined_sample[:,s2_index])
